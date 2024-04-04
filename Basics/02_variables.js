3899993cbdb9a621144c967f9951b17c4c547494
Shubham kumar debnath <162008551+ShubhamKD29@users.noreply.github.com>
2024-04-04 13:48:42 +0000

    practice about variables

---
+++ Basics/02_variables.js
@@ -0,0 +1,7 @@
+const rollNo = "BWU/BCA/21/263";
+let studentName = "Shubham Kumar Debnath";
+var cityName = "Mumbai";
+pincode = "400005";
+
+console.table([rollNo,studentName,cityName,pincode]);
+
