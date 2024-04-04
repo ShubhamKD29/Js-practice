9b58ca170e2b5442bb20611892dcbb9c47c15f4a
Shubham kumar debnath <162008551+ShubhamKD29@users.noreply.github.com>
2024-04-04 15:24:44 +0000

    data types

--- Basics/02_variables.js
+++ Basics/02_variables.js
@@ -2,6 +2,13 @@ const rollNo = "BWU/BCA/21/263";
 let studentName = "Shubham Kumar Debnath";
 var cityName = "Mumbai";
 pincode = "400005";
+let address;
+let code = null;
+let a = " ";
+console.table([rollNo,studentName,cityName,pincode,address,code]);
 
-console.table([rollNo,studentName,cityName,pincode]);
-
+console.log(typeof code);
+console.log(typeof address);
+console.log(typeof pincode);
+console.log(typeof cityName);
+console.log(typeof a);
